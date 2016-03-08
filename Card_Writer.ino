d6a13abdf3469357d33f33f169b24954034fbc07
seth.yang <sethyang@119app.com>
2016-03-09 03:41:47 +0800

    add function: write_sector, writeMark and writeData at file: card.h add module: write_card at file: Card_Writer.ino

--- Card_Writer.ino
+++ Card_Writer.ino
@@ -2,12 +2,18 @@
 #include "command.h"
 #include "card.h"
 
+#define ACTION_READ        'R'
+#define ACTION_WRITE       'W'
+
 RFID rfid (SDA_PIN, RST_PIN);
 Card card (&rfid);
 Command cmd;
 
 void setup () {
     Serial.begin(9600);
+
+    pinMode (BEEPER, OUTPUT);
+    digitalWrite (BEEPER, LOW);
 }
 
 void loop() {
@@ -18,33 +24,58 @@ void loop() {
 }
 
 /*########################## implementions ############################ */
+void read_card () {
+    unsigned char buff[32];
+    memset (buff, 0, 32);
+    if (card.readCard (buff) == MI_OK) {
+        int sum = 33 + ACTION_READ;
+        for (int i = 0; i < 32; i ++) sum += buff [i] & 0xff;
+        sum &= 0xff;
+        Serial.write (0xca);             // header 0
+        Serial.write (0xfe);             // header 1
+        Serial.write (1);                // state = success
+        Serial.write (ACTION_READ);      // type = read card
+        Serial.write (32);               // length
+        Serial.write (buff, 32);         // data
+        Serial.write (sum);              // CRC
+        Serial.write (0xba);             // tail
+        Serial.write (0xbe);
+
+        beep (BEEPER);
+    } else {
+        error (BEEPER);
+    }
+}
+
+void write_card (struct CNG_DATA *data) {
+    int state = card.writeMark (
+        data -> id, 
+        data -> timestamp, 
+        data -> main_version, 
+        data -> min_version
+    );
+    
+    if (state == MI_OK) {
+        if (card.writeData (
+                data -> isAdmin, 
+                data ->expire, 
+                data -> cardNo
+            ) == MI_OK) {
+            beep (BEEPER);
+            return;
+        }
+    }
+    error (BEEPER);
+}
+
 void process_command () {
     if (cmd.check_command ()) {
         switch (cmd.command.data.action) {
-            case ACTION_READ : {
-                unsigned char buff[32];
-                memset (buff, 0, 32);
-                if (card.readCard (buff) == MI_OK) {
-                    int sum = 33 + ACTION_READ;
-                    for (int i = 0; i < 32; i ++) sum += buff [i] & 0xff;
-                    sum &= 0xff;
-                    Serial.write (0xca);             // header 0
-                    Serial.write (0xfe);             // header 1
-                    Serial.write (1);                // state = success
-                    Serial.write (ACTION_READ);      // type = read card
-                    Serial.write (32);               // length
-                    Serial.write (buff, 32);         // data
-                    Serial.write (sum);              // CRC
-                    Serial.write (0xba);             // tail
-                    Serial.write (0xbe);
-
-                    beep (BEEPER);
-                } else {
-                    error (BEEPER);
-                }
+            case ACTION_READ :
+                read_card ();
                 break;
-            }
-            case ACTION_WRITE :
+            case ACTION_WRITE : 
+                write_card (&(cmd.command.data));
                 break;
             default :
                 break;
--- card.h
+++ card.h
@@ -15,9 +15,6 @@
 #define APP_SECTOR          14
 #define DATA_BLOCK_INDEX     2
 
-#define ACTION_READ        'R'
-#define ACTION_WRITE       'W'
-
 struct _position {
     int sector;
     int block;
@@ -33,13 +30,16 @@ struct _position {
 class Card {
     private :
         RFID *rfid;
-        unsigned char SN[5];
         struct _position mark, app;
 
-        int read_sector (struct _position *pos, unsigned char* key, unsigned char *buff);
+        int read_sector (struct _position *pos, unsigned char *key, unsigned char *buff);
+        int write_sector (struct _position *pos, unsigned char *key, unsigned char *data);
     public :
         Card (RFID *rfid);
         int readCard (unsigned char *buff);
+        int writeMark (uint16_t index, uint32_t timestamp, uint8_t mainVersion, uint8_t minVersion);
+        int writeData (uint8_t isAdmin, uint32_t expire, uint32_t userId);
+        
         unsigned char DEFAULT_KEY[16] = {
             0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
             0xff, 0x07, 0x80, 0x69,
@@ -56,9 +56,6 @@ Card::Card (RFID *rfid) {
     this->rfid = rfid;
     SPI.begin();
 
-    pinMode (BEEPER, OUTPUT);
-    digitalWrite (BEEPER, LOW);
-
     mark.sector = MARK_SECTOR;
     app.sector  = APP_SECTOR;
     app.block   = mark.block = DATA_BLOCK_INDEX;
@@ -71,6 +68,8 @@ Card::Card (RFID *rfid) {
 int Card::read_sector (struct _position *pos, unsigned char *key, unsigned char *buff) {
     rfid -> init ();
     rfid -> isCard ();
+    unsigned char SN[5];
+    memset (SN, 0, 5);
     if (rfid -> readCardSerial ()) {
         memcpy (SN, rfid->serNum, 5);
     }
@@ -78,6 +77,24 @@ int Card::read_sector (struct _position *pos, unsigned char *key, unsigned char 
     int state = rfid->auth (PICC_AUTHENT1A, pos -> password_offset (), key, SN);
     if (state == MI_OK) {
         return rfid -> read (pos -> data_offset (), buff);
+    }
+}
+
+/**
+ * write the data to block as pos with key
+ */
+int Card::write_sector (struct _position *pos, unsigned char *key, unsigned char *data) {
+    rfid -> init ();
+    rfid -> isCard ();
+    unsigned char SN[5];
+    memset (SN, 0, 5);
+    if (rfid -> readCardSerial ()) {
+        memcpy (SN, rfid->serNum, 5);
+    }
+    rfid -> selectTag (SN);
+    int state = rfid->auth (PICC_AUTHENT1A, pos -> password_offset (), key, SN);
+    if (state == MI_OK) {
+        return rfid -> write (pos -> data_offset (), data);
     }
 }
 
@@ -91,5 +108,51 @@ int Card::readCard (unsigned char *buff) {
     }
     
     return read_sector (&app, DEFAULT_KEY, buff + 16);
+}
+
+/**
+ * 
+ */
+int Card::writeMark (uint16_t index, uint32_t timestamp, uint8_t mainVersion, uint8_t minVersion) {
+    unsigned char buff[16] = {0xca, 0xfe, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0xba, 0xbe};
+    // index
+    buff [2] = (index >> 8) & 0xff;
+    buff [3] = index & 0xff;
+
+    // timestamp
+    buff [4] = (timestamp >> 24) & 0xff;
+    buff [5] = (timestamp >> 16) & 0xff;
+    buff [6] = (timestamp >>  8) & 0xff;
+    buff [7] = timestamp         & 0xff;
+
+    // main version
+    buff [8] = mainVersion;
+    buff [9] = minVersion;
+
+    return write_sector (&mark, DEFAULT_KEY, buff);
+}
+
+/**
+ * 
+ */
+int Card::writeData (uint8_t isAdmin, uint32_t expire, uint32_t userId) {
+    unsigned char buff[16] = {0xca, 0xfe, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0xba, 0xbe};
+
+    // admin
+    buff [2] = isAdmin;
+
+    // expire
+    buff [3] = (expire >> 24) & 0xff;
+    buff [4] = (expire >> 16) & 0xff;
+    buff [5] = (expire >>  8) & 0xff;
+    buff [6] = expire         & 0xff;
+
+    // user id
+    buff [ 7] = (userId >> 24) & 0xff;
+    buff [ 8] = (userId >> 16) & 0xff;
+    buff [ 9] = (userId >>  8) & 0xff;
+    buff [10] = userId         & 0xff;
+
+    return write_sector (&app, DEFAULT_KEY, buff);
 }
 #endif // CARD_H
